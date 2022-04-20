6a882a4b286a8f11641d2f991efd103ebfb46e7f
Dilgam Israfilov <dilqemisrafil@gmail.com>
2022-04-20 10:17:53 +0400

    garbage 1

---
+++ garbage.js
@@ -0,0 +1,11 @@
+function smartGarbage (trash,bins){
+if (trash == "waste"){
+bins.waste = bins.waste + 1 
+}else if (trash == "recycling"){
+bins.recycling = bins.recycling + 1;
+} else if (trash == "compost"){
+bins.compost = bins.compost + 1;
+}
+return bins
+}
+console.log(smartGarbage('waste', {waste: 4, recycling: 3, compost: 5}))
